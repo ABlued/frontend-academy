574673f7e2ff9713b993fa1057297ed0755ed444
NaJaeWan <2015132011@kpu.ac.kr>
2024-02-15 22:16:56 +0900

    feat : implement router

--- Part3 코드/CH03_02_12줄의 코드로 시작하기/app.js
+++ Part3 코드/CH03_02_12줄의 코드로 시작하기/app.js
@@ -11,14 +11,28 @@ function getData(url) {
   return JSON.parse(ajax.response);
 }
 
-const newsFeed = getData(NEWS_URL);
-const ul = document.createElement('ul');
+function newsFeed() {
+  const newsList = [];
+  const newsFeed = getData(NEWS_URL);
 
-window.addEventListener('hashchange', function () {
+  newsList.push('<ul>');
+  for (let i = 0; i < 10; i++) {
+    newsList.push(`
+    <li>
+      <a href="#${newsFeed[i].id}">${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
+    </li>
+  `);
+  }
+  newsList.push('</ul>');
+
+  container.innerHTML = newsList.join('');
+  container.appendChild(content);
+}
+
+function newsDetail() {
   const id = location.hash.substring(1);
 
   const newsContent = getData(CONTENT_URL.replace('@id', id));
-  const title = document.createElement('h1');
   container.innerHTML = '';
   container.innerHTML = `
     <h1>${newsContent.title}</h1>
@@ -26,20 +40,16 @@ window.addEventListener('hashchange', function () {
       <a href="#">목록으로</a>
     </div>
   `;
-});
-
-const newsList = [];
-
-newsList.push('<ul>');
-
-for (let i = 0; i < 10; i++) {
-  newsList.push(`
-    <li>
-      <a href="#${newsFeed[i].id}">${newsFeed[i].title} (${newsFeed[i].comments_count})</a>
-    </li>
-  `);
 }
-newsList.push('</ul>');
 
-container.innerHTML = newsList.join('');
-container.appendChild(content);
+function router() {
+  const routePath = location.hash;
+  if (routePath === '') {
+    newsFeed();
+  } else {
+    newsDetail();
+  }
+}
+
+window.addEventListener('hashchange', router);
+router();
