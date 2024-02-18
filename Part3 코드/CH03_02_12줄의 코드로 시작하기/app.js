ffc5b8a139a501819841430b30c48044cf6d6475
NaJaeWan <2015132011@kpu.ac.kr>
2024-02-18 20:46:39 +0900

    feat : apply template rendering

--- Part3 코드/CH03_02_12줄의 코드로 시작하기/app.js
+++ Part3 코드/CH03_02_12줄의 코드로 시작하기/app.js
@@ -3,6 +3,7 @@ let ajax = new XMLHttpRequest();
 const content = document.createElement('div');
 const NEWS_URL = 'https://api.hnpwa.com/v0/news/1.json';
 const CONTENT_URL = 'https://api.hnpwa.com/v0/item/@id.json';
+const PAGE_SIZE = 10;
 const store = {
   currentPage: 1,
 };
@@ -16,10 +17,24 @@ function getData(url) {
 function newsFeed() {
   const newsFeed = getData(NEWS_URL);
   const newsList = [];
+  let template = `
+    <div class="container mx-auto p-4">
+      <h1>Hacker News</h1>
+      <ul>
+        {{__news_feed__}}      
+      </ul>
+      <div>
+        <a href="#/page/{{__prev_page__}}">이전 페이지</a>
+        <a href="#/page/{{__next_page__}}">다음 페이지</a>
+      </div>
+    </div>
+  `;
 
-  newsList.push('<ul>');
-
-  for (let i = (store.currentPage - 1) * 10; i < store.currentPage * 10; i++) {
+  for (
+    let i = (store.currentPage - 1) * PAGE_SIZE;
+    i < store.currentPage * PAGE_SIZE;
+    i++
+  ) {
     newsList.push(`
       <li>
         <a href="#/show/${newsFeed[i].id}">
@@ -29,20 +44,17 @@ function newsFeed() {
     `);
   }
 
-  newsList.push('</ul>');
-  newsList.push(`
-    <div>
-      <a href="#/page/${
-        store.currentPage > 1 ? store.currentPage - 1 : 1
-      }">이전 페이지</a>
-      <a href="#/page/${Math.min(
-        store.currentPage + 1,
-        newsFeed.length / 10
-      )}">다음 페이지</a>
-    </div>
-  `);
+  template = template.replace('{{__news_feed__}}', newsList.join(''));
+  template = template.replace(
+    '{{__prev_page__}}',
+    store.currentPage > 1 ? store.currentPage - 1 : 1
+  );
+  template = template.replace(
+    '{{__next_page__}}',
+    Math.min(store.currentPage + 1, newsFeed.length / PAGE_SIZE)
+  );
 
-  container.innerHTML = newsList.join('');
+  container.innerHTML = template;
 }
 
 function newsDetail() {
